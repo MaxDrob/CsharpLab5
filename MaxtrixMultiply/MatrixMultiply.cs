aad6d158e12ed8c7d4102f4bedd836f206fcf363
MaxDrob <83185232+MaxDrob@users.noreply.github.com>
2021-05-17 22:14:08 +0300

    Add project files.

---
+++ MaxtrixMultiply/MatrixMultiply.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixMultiply
+{
+    class MatrixMultiply
+    {
+        
+        static void Output(int[,] result)
+        {
+            for (int r = 0; r < result.GetLength(0); r++)
+            {
+                for (int c = 0; c < result.GetLength(1); c++)
+                {
+                    Console.Write("{0} ", result[r, c]);
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        static int[,] Multiply(int[,] a, int[,] b)
+        {
+            int[,] result = new int[2, 2];
+            for (int r = 0; r < 2; r++)
+            {
+                for (int c = 0; c < 2; c++)
+                {
+                    result[r, c] = a[r, 0] * b[0, c] + a[r, 1] * b[1, c];
+                }
+            }
+            return
+                result;
+        }
+        
+        static void Input(int[,] dst)
+        {
+            for (int r = 0; r < dst.GetLength(0); r++)
+            {
+                for (int c = 0; c < dst.GetLength(1); c++)
+                {
+                    Console.Write("Enter element of matrix for [{0},{1}]: ", r, c);
+                    string s = Console.ReadLine();
+                    dst[r, c] = int.Parse(s);
+                }
+            }
+            Console.WriteLine();
+
+        }
+        static void Main(string[] args)
+        {
+            int[,] a = new int[2, 2];
+            int[,] b = new int[2, 2];
+            Input(a);
+            Input(b);
+            int[,] result = Multiply(a, b);
+            Output(result);
+
+            Console.ReadKey();
+        }
+    }
+}
